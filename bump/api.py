02f8746ffdb7617edbbb97c4693734152c74607e
spozdniakov <106153129+spozdniakov@users.noreply.github.com>
2022-05-28 13:40:07 +0300

    3 Lesson

--- bump/api.py
+++ bump/api.py
@@ -1,3 +1,4 @@
+import inspect
 from parse import parse
 from webob import Request, Response
 
@@ -14,6 +15,8 @@ class API:
         return response(environ, start_response)
 
     def route(self, path):
+        assert path not in self.routes, "Such route already exists."
+
         def wrapper(handler):
             self.routes[path] = handler
             return handler
@@ -38,6 +41,11 @@ class API:
         handler, kwargs = self.find_handler(request_path=request.path)
 
         if handler is not None:
+            if inspect.isclass(handler):
+                handler = getattr(handler(), request.method.lower(), None)
+                if handler is None:
+                    raise AttributeError("Method not allowed", request.method)
+
             handler(request, response, **kwargs)
         else:
             self.default_response(response)
